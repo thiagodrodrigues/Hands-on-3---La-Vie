8943bbb4c44c6382651121312af07805cfa7b1c3
PatrickSegalla <patricksegalla22@gmail.com>
2022-09-16 09:09:37 -0300

    Continuação do CRUD Atendimentos

--- controllers/atendimentosController.js
+++ controllers/atendimentosController.js
@@ -1,33 +1,58 @@
-const atendimentos = require('../models/atendimentos')
+const Atendimentos = require("../models/atendimentos");
 
 const atendimentosController = {
-
-    async listAtendimentos (req, res) {
-
-        const listaDeAtendimentos = await atendimentos.findAll();
-
-        res.json(listaDeAtendimentos);
-    
-    },
-
-    async getAtendimentoId (req, res) {
-
-        const { idAtendimento } = req.params;
-
-        await atendimentos.findAll();
-
-        res.json (idAtendimento);
-    },
-
-    async postAtendimento (req, res) {
-        const {paciente_id, data_atendimento, observacao} = req.body;
-
-        const novoAtendimento = await atendimentos.create({
-            paciente_id, data_atendimento, observacao
+    async listarAtendimentos (req,res){
+        try {
+        let array = [];
+        const listaDeAtendimentos = await Atendimentos.findAll({
         });
 
-        res.json (novoAtendimento)
+        if(listaDeAtendimentos.length==0){
+            return res.status(400).json(array);
+        }
+        res.status(200).json(listaDeAtendimentos);
+
+        } catch (error) {
+            console.error(error);
+            res.status(400).send("Houve um erro.")
+        }
     },
+
+    async listaAtendimentoID(req,res){
+        try {
+            const { id } = req.params;
+            const referenciaAtendimento = await Atendimentos.findOne({
+                where:{
+                    idAtendimento : id
+                }
+            });
+
+            if(!referenciaAtendimento){
+                return res.status(404).send("ID do atendimento não foi encontrado.")
+            } else {
+                return res.status(200).json(referenciaAtendimento);}
+        } catch (error) {
+            console.error(error);
+            return res.status(400).send("Ocorreu um erro.");
+        };
+    },
+
+    async cadastrarAtendimento(req,res){
+        try {
+            const {paciente_id, data_atendimento, observacao} = req.body;
+
+            const novoAtendimento = await Atendimentos.create({
+                paciente_id,
+                data_atendimento,
+                observacao,
+            });
+            return res.status(201).json(novoAtendimento);
+        } catch (error) {
+            console.error(error);
+            res.status(404).send("Ocorreu um erro.");
+        };
+    },
+
 };
 
 module.exports = atendimentosController;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const routes = express.Router()
+const atendimentosController = require ("../controllers/atendimentosController");
 
 /* routes.get('/gender', GenderController.listAll) -> Apenas deixei de exemplo */
 
-routes.get('atendimentos', atendimentosController.listAtendimentos);
-routes.get('atendimentos/:id', atendimentosController.getAtendimentoId);
-routes.post('atendimentos', atendimentosController.postAtendimento);
+routes.get('/atendimentos', atendimentosController.listarAtendimentos);
+routes.get('/atendimentos/:id', atendimentosController.listaAtendimentoID);
+routes.post('/atendimentos', atendimentosController.cadastrarAtendimento); 
 
 
 module.exports = routes
